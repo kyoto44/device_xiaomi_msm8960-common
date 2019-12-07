4e9807a5add827e8cdde93b926d30fed7cab2a67
Dmitry Gomzyakov <nicerok11@gmail.com>
2019-12-07 12:24:12 +0500

    msm8960-common: fix camera build

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8960 \
-    libmmcamera_interface2 \
     Snap
 
 PRODUCT_COPY_FILES += \
